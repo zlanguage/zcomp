3ffc9af704718ab8cebbd6588db4d222ba1ce540
N8 <n8@n8programs.com>
2019-07-21 17:03:37 -0400

    Version 0.0.12

--- src/compiler/tokenize.js
+++ src/compiler/tokenize.js
@@ -34,7 +34,7 @@ Capturing Group:
 [6] Number
 [7] Punctuator
 */
-const tokenRegExp = /(\u0020+)|(#.*)|("(?:[^"\\]|\\(?:[nr"\\]|u\{[0-9A-F]{4,6}\}))*")|\b(let|loop|if|elif|else|func|break|import|export|match|return|def|=>|try|on|settle|raise|importstd)\b|([A-Za-z_+\-/*%&|?^=<>'!][A-Za-z0-9+\-/*%&|?^<>='!]*)|(-?[\d_]+(?:\.[\d_]+)?(?:e\-?[\d_]+)?)|([(),.{}\[\]:])/y;
+const tokenRegExp = /(\u0020+|\t+)|(#.*)|("(?:[^"\\]|\\(?:[nr"\\]|u\{[0-9A-F]{4,6}\}))*")|\b(let|loop|if|elif|else|func|break|import|export|match|return|def|=>|try|on|settle|raise|importstd)\b|([A-Za-z_+\-/*%&|?^=<>'!][A-Za-z0-9+\-/*%&|?^<>='!]*)|(-?[\d_]+(?:\.[\d_]+)?(?:e\-?[\d_]+)?)|([(),.{}\[\]:])/y;
 /**
  * Create a token generator for a specific source.
  * @param {string | Array<string>} source If string is provided, string is split along newlines. If array is provided, array is used as the array of lines.
