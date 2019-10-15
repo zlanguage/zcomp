0907a24d0a3c11868c1c8161d8ffe78771e0167b
Reece Dunham <me@rdil.rocks>
2019-10-15 12:25:28 -0400

    Properly post applications, prevent duplicates

--- src/compiler/plugins/PluginManager.js
+++ src/compiler/plugins/PluginManager.js
@@ -1,22 +1,28 @@
 const plug = require("./Plugin")
 const types = require("./EventTypes")
 
-/**
- * Loaded plugins
- * DO NOT MODIFY DIRECTLY, USE `apply(plugin)`
- */
 let loadedPlugins = []
 
 export default function apply(plugin) {
   if(!plugin instanceof plug.Plugin) {
     console.error("Failed to apply plugin as the type was incorrect.")
   } else {
-    loadedPlugins.append(plugin)
+    val safe = true
+    loadedPlugins.forEach(pi => {
+      if(pi == plugin) {
+        safe = false
+      }
+    })
+    if(!safe) {
+      console.error("Duplicate plugin application detected. Proceeed with caution!")
+    } else {
+      loadedPlugins.append(plugin)
+    }
   }
 }
 
 export function triggerLoads() {
-  loadedPlugins.forEach(it => it.onApply(new types.PluginApplyEvent(it)))
+  loadedPlugins.forEach(it => loadedPlugins.forEach(other => other.onApply(new types.PluginApplyEvent(it))))
 }
 
 export function triggerEvent(event) {
