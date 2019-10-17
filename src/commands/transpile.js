94ae09023393fb80ed4b5e256ace01afc5612852
Reece Dunham <me@rdil.rocks>
2019-10-16 21:51:24 -0400

    properly trigger file compile event upon command

--- src/commands/transpile.js
+++ src/commands/transpile.js
@@ -1,39 +1,46 @@
-const { Command, flags } = require("@oclif/command");
-const tokenize = require("../compiler/tokenize");
-const parse = require("../compiler/parse");
-const gen = require("../compiler/gen");
+const { Command, flags } = require("@oclif/command")
+const tokenize = require("../compiler/tokenize")
+const parse = require("../compiler/parse")
+const gen = require("../compiler/gen")
 const fs = require("fs");
+
 class TranspileCommand extends Command {
   async run() {
-    const { args } = this.parse(TranspileCommand);
-    let { path, to } = args;
+    const { args } = this.parse(TranspileCommand)
+    let { path, to } = args
     if (!path) {
-      throw new Error("Transpilation expects path.");
+      throw new Error("Transpilation expects path.")
     }
     if (!to) {
-      to = path.replace(/(.+).zlang/, "$1.js");
+      to = path.replace(/(.+).zlang/, "$1.js")
     }
     fs.readFile(path, (err, data) => {
       if (err) {
-        return this.log(err);
+        return this.log(err)
       }
-      let res;
+      let res = ""
+      const eventTypes = require("../compiler/plugins/EventTypes")
+      let e = new eventTypes.CompileFileEvent(path)
+      if (e.isCancelled) {
+        return
+      }
       try {
-        res = gen(parse(tokenize(data.toString())));
+        res = gen(parse(tokenize(data.toString())))
       } catch (err) {
-        this.log(err);
+        this.log(err)
       }
       fs.writeFile(to, res, err => {
         if (err) {
-          this.log(err);
+          this.log(err)
         }
       });
     });
   }
 }
 
-TranspileCommand.description = `Transpiles files from Z into JS
-...
+TranspileCommand.description = `Transpiles given file from Z to JS
+
+Options:
 path: Path of file to transpile
 to: Where to transpile the file
 `;
@@ -41,5 +48,6 @@ to: Where to transpile the file
 TranspileCommand.args = [
   { name: "path", description: "Path of file to transpile", required: true },
   { name: "to", description: "Where to transpile the file.", required: false }
-];
-module.exports = TranspileCommand;
+]
+
+module.exports = TranspileCommand
