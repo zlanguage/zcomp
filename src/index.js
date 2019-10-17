0fe7dac9510cf2d4fc93164642e72d1289022176
Reece Dunham <me@rdil.rocks>
2019-10-17 15:09:44 -0400

    tweaked stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 const pluginMan = require("./compiler/plugins/PluginManager")
-
-let jsons = []
-
-export function addConfig(configDict) {
-  jsons.append(configDict)
-}
+const fs = require('fs')
 
 function startUp() {
-  jsons["pluginClasses"].forEach(pc => {
-    pluginMan.apply(require(pc))
-  })
+  let filename = require("path").join(process.cwd() + ".zconfigs.json")
+  if (fileExists(filename)) {
+    let json = ""
+    fs.readFile(filename, (errorContext, data) => {
+      if (errorContext) {
+        console.err(errorContext)
+      } else {
+        json = JSON.parse(data.toString())
+      }
+    })
+    try {
+      json["pluginClasses"].forEach(pc => {
+        try {
+          pluginMan.apply(require(pc))
+        } catch (ex) {
+          console.error("Plugin apply phase error: " + ex)
+        }
+      })
+    } catch (i) {
+    }
+  }
   return require("@oclif/command")
+}
+
+let fileExists = filename => {
+  fs.access(filename, fs.F_OK, (err) => {
+    // keep it like this, DO NOT TRY to return `err`!!!
+    if (err) {
+      return false
+    }
+    return true
+  })
 }
 
 export default startUp()
